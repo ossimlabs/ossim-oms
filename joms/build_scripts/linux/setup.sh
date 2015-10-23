74777b2200659eca3711f30e9da0bfc47c00840d
Garrett Potts <gpotts@radiantblue.com>
2015-10-23 15:02:57 -0400

    Fixed to test if GROOVY_VERSION is installed

--- joms/build_scripts/linux/setup.sh
+++ joms/build_scripts/linux/setup.sh
@@ -5,6 +5,10 @@ fi
 
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
+if [ -z "$GROOVY_VERSION" ]; then
+   export GROOVY_VERSION=2.4.0
+fi
+
 if [ ! -d "$HOME/.sdkman/groovy/$GROOVY_VERSION" ]; then
    sdk install groovy $GROOVY_VERSION
 fi
