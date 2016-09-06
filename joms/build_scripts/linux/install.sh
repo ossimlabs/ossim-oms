c56d38cd48db00a0e6663cdf553d9438b1d9bec9
Garrett Potts <gpotts@radiantblue.com>
2016-09-06 12:57:31 -0400

    Added similar setup to install as for build.sh

--- joms/build_scripts/linux/install.sh
+++ joms/build_scripts/linux/install.sh
@@ -7,6 +7,18 @@ popd >/dev/null
 
 
 pushd $JOMS_HOME > /dev/null
+if [ ! -a local.properties ]
+   then 
+   cp local.properties.template local.properties
+fi
+
+if [ -z "$GROOVY_HOME" ]; then
+   echo "HOME Location: ${HOME}"
+   source "$HOME/.sdkman/bin/sdkman-init.sh"
+   if [ ! -z "$GROOVY_VERSION" ]; then
+      sdk use groovy $GROOVY_VERSION
+   fi
+fi
 
 ant install
 antReturnCode=$?
