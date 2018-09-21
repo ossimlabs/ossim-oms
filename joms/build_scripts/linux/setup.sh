bdceb4ff2e98951ad287d63d866cb4336821998c
Garrett Potts <gpotts@radiantblue.com>
2018-09-21 07:50:14 -0400

    Removed the auto install process and just added a check for groovy executable in the path

--- joms/build_scripts/linux/setup.sh
+++ joms/build_scripts/linux/setup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-echo "Checking for sdkman: $HOME/.sdkman"
-if [ ! -d "$HOME/.sdkman" ]; then
-   echo "Installing sdkman....."
-   curl -s get.sdkman.io | bash
-   echo "Done installing sdkman....."
-fi
+# echo "Checking for sdkman: $HOME/.sdkman"
+# if [ ! -d "$HOME/.sdkman" ]; then
+#    echo "Installing sdkman....."
+#    curl -s get.sdkman.io | bash
+#    echo "Done installing sdkman....."
+# fi
 
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+# source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-if [ -z "$GROOVY_VERSION" ]; then
-   export GROOVY_VERSION=2.4.0
-fi
+# if [ -z "$GROOVY_VERSION" ]; then
+#    export GROOVY_VERSION=2.4.0
+# fi
 
-if [ ! -d "$HOME/.sdkman/groovy/$GROOVY_VERSION" ]; then
-   sdk install groovy $GROOVY_VERSION
+# if [ ! -d "$HOME/.sdkman/groovy/$GROOVY_VERSION" ]; then
+#    sdk install groovy $GROOVY_VERSION
+# fi
+groovy --version
+if [ $? -ne 0 ]; then
+   echo "groovy is not installed on this machine.  Please install groovy!"
 fi
