94462facd5c0d70dce6bb11636bb82cd75dad4b6
Garrett Potts <gpotts@radiantblue.com>
2019-09-11 22:24:03 -0400

    Added the env propterty

--- joms/build_scripts/linux/env.sh
+++ joms/build_scripts/linux/env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 pushd `dirname ${BASH_SOURCE[0]}` >/dev/null
-export SCRIPT_DIR=`pwd -P`
+export ENV_SCRIPT_DIR=`pwd -P`
 popd >/dev/null
 
-pushd $SCRIPT_DIR/../.. >/dev/null
+pushd $ENV_SCRIPT_DIR/../.. >/dev/null
 export JOMS_HOME=$PWD
-echo "@@@@@ SCRIPT_DIR=$SCRIPT_DIR"
+echo "@@@@@ SCRIPT_DIR=$ENV_SCRIPT_DIR"
 echo "@@@@@ JOMS_HOME=$JOMS_HOME"
 popd >/dev/null
 
