b1d81abd2743407e4a25ab58b004e5eb02df50cb
sbortman <scott.bortman@gmail.com>
2020-08-29 10:48:34 -0400

    include ALL of the .o files

--- joms/fix-libjoms.sh
+++ joms/fix-libjoms.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gcc -shared -Wall swig/oms_wrap.o -o libjoms.so -L$OSSIM_INSTALL_PREFIX/lib64 -lossim -loms
+gcc -shared -Wall swig/*.o -o libjoms.so -L$OSSIM_INSTALL_PREFIX/lib64 -lossim -loms
 cp libjoms.so $OSSIM_INSTALL_PREFIX/lib64
